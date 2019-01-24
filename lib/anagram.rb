29724e227a49a8920f3e6d2d7dc9c220069970c3
Trent Moss <trentmoss05@gmail.com>
2019-01-24 21:56:13 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.each do |word_to_match|
+      letters = []
+      letters = word_to_match.split("")
+      if
+end
